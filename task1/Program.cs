7b8d098e0f2aace77a08ccd65cf967ffd3036068
diplg <dimas4354@icloud.com>
2022-05-06 00:38:41 +0300

    Задание на подстчёт положительных чисел

---
+++ task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите количество проверяемых чисел: ");
+int index = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < index; i++)
+{
+    Console.Write("Введите число " + (i + 1) + " = ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number > 0) count += 1;
+}
+Console.WriteLine("Количество введенных чисел > 0 равно: " + count);
